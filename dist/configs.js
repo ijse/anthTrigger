a58cebab6bff8b85b671077f1326a9879102b8cc
ijse <i@ijser.cn>
2014-07-17 19:01:04 +0800

    clean codes

--- dist/configs.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-	listen: 5678,
-	shellDir: __dirname + '/shells',
-	dbDir: __dirname + '/database'
-}
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-
-require('coffee-script/register')
-
-global.__config = configs = require('./configs')
-global.__util = require('./lib/utils')
-
-var server = require('./lib/server')(configs)
-
-server.listen(configs.listen)
