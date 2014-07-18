cd540c72aaa717e1d1673f9165e5a48ee439eff9
ijse <i@ijser.cn>
2014-07-18 13:48:12 +0800

    read mongodb url config from env

--- configs.js
+++ configs.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	listen: process.env.VCAP_APP_PORT || process.env.PORT || 5678,
 
-	mongodb: "mongodb://127.0.0.1:27017/anthTrigger",
+	mongodb: process.env.MONGODB || "mongodb://127.0.0.1:27017/anthTrigger",
 	shellDir: __dirname + '/shells',
 	dbDir: __dirname + '/database'
 }
