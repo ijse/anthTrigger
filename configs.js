288b143ceebbd832a75435aa5501d814504c5827
ijse <i@ijser.cn>
2014-07-24 12:49:37 +0800

    update configs

--- configs.js
+++ configs.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	listen: process.env.VCAP_APP_PORT || process.env.PORT || 5678,
 
-	mongodb: process.env.MONGODB || "mongodb://root:sa@kahana.mongohq.com:10060/anthTrigger",
+	mongodb: process.env.MONGODBURL || "mongodb://root:sa@kahana.mongohq.com:10060/anthTrigger",
 	shellDir: __dirname + '/shells',
 	dbDir: __dirname + '/database',
 
